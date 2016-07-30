4a95f2590511d7575d4e7b04111ea756bd063d54
guigas <guigashm@hotmail.com>
2016-07-30 09:48:45 +0100

    V1.1

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-bluetooth-serial.bluetoothSerial",
+        "file": "plugins/cordova-plugin-bluetooth-serial/www/bluetoothSerial.js",
+        "pluginId": "cordova-plugin-bluetooth-serial",
+        "clobbers": [
+            "window.bluetoothSerial"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-bluetooth-serial": "0.4.5"
+};
+// BOTTOM OF METADATA
+});
