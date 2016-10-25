c3c4e7adb93911591b6b0c6f7cc830490f148e11
guigas <guigashm@hotmail.com>
2016-10-26 00:56:42 +0100

    V2.4

--- serial_RPM_tester/serial_RPM_tester.ino
+++ serial_RPM_tester/serial_RPM_tester.ino
@@ -2,16 +2,21 @@
 #include <SoftwareSerial.h>
 SoftwareSerial BluetoothSerial(11, 10); // RX, TX
 
+int FuelLevelPin = A0;
+int FuelLevelValue = 290; // 290 is full - 280 empty
 int count = 0;
 int i = 0;
 
 void setup() {
+
+  pinMode(FuelLevelPin, INPUT);
+
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  BluetoothSerial.begin(38400);
+  BluetoothSerial.begin(115200);
   delay(10000);
   Serial.println("Honda ALD 2 BT RPM tester");
   BluetoothSerial.println("Honda ALD 2 BT RPM tester");
@@ -19,21 +24,53 @@ void setup() {
 
 void loop() {
 
-  if (count < 101 and i == 0) {
-    Serial.print("RPM:");
+  FuelLevelValue = analogRead(FuelLevelPin) - 230; 
+
+  if (count < 10000 and i == 0) {
+    Serial.print("FuelLevel:");
+    Serial.print(FuelLevelValue);
+    Serial.print("spd");
+    Serial.print((count / 30));
+    Serial.print("rpm");
     Serial.println(count);
-    BluetoothSerial.print(count);
-    delay(1000);  // wait for a second
-    count++;  // add one (1) to our count
+    BluetoothSerial.print("thr");
+    BluetoothSerial.print(90);
+    BluetoothSerial.print("vlt");
+    BluetoothSerial.print(12);
+    BluetoothSerial.print("flv");
+    BluetoothSerial.print(FuelLevelValue);
+    BluetoothSerial.print("tco");
+    BluetoothSerial.print(70);
+    BluetoothSerial.print("spd");
+    BluetoothSerial.print((count / 30));
+    BluetoothSerial.print("rpm");
+    BluetoothSerial.println(count);
+    delay(50);  // wait for half a second
+    count = count + 100;  // add one (1) to our count
   } else {
     if (i == 0) {
       i = count;
     }
-    Serial.print("RPM:");
+    Serial.print("FuelLevel:");
+    Serial.print(FuelLevelValue);
+    Serial.print("spd");
+    Serial.print((count / 30));
+    Serial.print("rpm");
     Serial.println(count);
-    BluetoothSerial.print(count + 'N');
-    delay(1000);  // wait for a second
-    count--;  // subtract one (1) to our count
-    i--;  // subtract one (1) to our count
+    BluetoothSerial.print("thr");
+    BluetoothSerial.print(90);
+    BluetoothSerial.print("vlt");
+    BluetoothSerial.print(12);
+    BluetoothSerial.print("flv");
+    BluetoothSerial.print(FuelLevelValue);
+    BluetoothSerial.print("tco");
+    BluetoothSerial.print(70);
+    BluetoothSerial.print("spd");
+    BluetoothSerial.print((count / 30));
+    BluetoothSerial.print("rpm");
+    BluetoothSerial.println(count);
+    delay(50);  // wait for half a second
+    count = count - 100;  // subtract one (1) to our count
+    i = i - 100;  // subtract one (1) to our count
   }
 }
